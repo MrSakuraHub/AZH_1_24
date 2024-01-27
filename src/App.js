6dc19be4cb29145f1269b7a0699ec99be7e94b76
C0C0Man <129965644+C0C0Man@users.noreply.github.com>
2024-01-27 15:04:41 -0500

    Taking figma code and remaking website from start Got through wallet display and buttons. Doing my best to impliment disign ny danielle

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import Navbar from './components/Navbar'
 import './index.css';
 import SignUp from './pages/SignUp';
 import Wallet from './pages/Wallet';
-<<<<<<< HEAD
-import './styles/style.css';
-import './styles/styleguide.css';
-=======
-import { FormProvider } from './context/FormContext';
-import Form from './formComponents/form';
->>>>>>> 815f82e954b7ee649195fc3c6afd44c8274c0fbd
+import WalletR from './pages/WalletReference';
+
+
 
 function App() {
   return(
--- src/components/WalletTotal.js
+++ src/components/WalletTotal.js
@@ -1,18 +1,48 @@
-
+import { Button } from "./Button/Button";
 
 function WalletTotal () {
     return(
-        <div>
-            <p>Total Balance</p>
-            <h1>1000 USD</h1> {/* This is where the call functions will go to get wallet balances */}
-            <p>-120 AZERO</p>
-            <div>
-                <button>Send Money</button> 
-                <button> Request  </button>
-                <button>   Refill  </button> 
-                <button>  History  </button>
+        <div className="wallet-display">
+            <div className="total-balance">
+              <div className="balance-heading"> Total Balance </div>
+              <div className="currency-display">
+                <div className="number-display"> 2.820 </div>
+                <div className="currency-type">USD</div>
+              </div>
+              <div className="azero-bal">~120 AZERO</div>
             </div>
-        </div>
+            <div className="button-wrapper">
+            <Button className="button-instance" 
+              priority="primary" 
+              property1="default" 
+              showIcon={false}
+              />
+              <Button
+                  className="button-instance"
+                  priority="secondary"
+                  property1="default"
+                  showIcon={false}
+                  text1="Request"
+                />
+                <Button
+                  className="button-instance"
+                  priority="secondary"
+                  property1="default"
+                  showIcon={false}
+                  text1="Refill"
+                />
+                <Button className="button-instance" 
+                  priority="secondary" 
+                  property1="default" 
+                  text1="Other action" 
+                />
+                <Button className="button-instance" 
+                  priority="secondary" 
+                  property1="default" 
+                  text1="History" 
+                />
+            </div>
+          </div>
     )
 }
 
