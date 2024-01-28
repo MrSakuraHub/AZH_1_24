49b4bb648408c1a57048a6b6860551406771acd6
C0C0Man <129965644+C0C0Man@users.noreply.github.com>
2024-01-28 16:34:59 -0500

    Finished skeleton framework and component styles

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,36 +1,14 @@
 import React from "react";
-import { Algo } from "../components/Algo";
-import { AlgoWrapper } from "../components/Algo/Algowrapper";
-import { ArchiveFiles } from "../icons/ArchiveFiles";
 import { Blob11 } from "../icons/Blob11";
 import { Blur } from "../components/Blur/Blur";
-import { Btc } from "../icons/Btc/Btc";
-import { BtcWrapper } from "../components/BtcWrapper/BtcWrapper";
-import { Button } from "../components/Button/Button";
-import { ChevronRight } from "../icons/ChevronRight";
-import { ClipboardAdd1 } from "../icons/ClipboardAdd1";
-import { Dropdown } from "../components/Dropdown/Dropdown";
-import { ExpandArrows } from "../icons/ExpandArrows";
-import { File } from "../icons/File";
-import { IconComponentNode } from "../components/IconComponentNode/IconComponentNode";
-import { ImageWandMagicRightStars1 } from "../icons/ImageWandMagicRightStars1";
-import { Link } from "../icons/Link";
-import { LinkWrapper } from "../components/LinkWrapper/LinkWrapper";
-import { MagnifyingGlass1 } from "../icons/MagnifyingGlass1";
-import { Music } from "../icons/Music";
-import { PhoneDialAlt } from "../icons/PhoneDialAlt";
-import { Refresh2 } from "../icons/Refresh2";
-import { Usdc } from "../icons/Usdc";
-import { UsdcWrapper } from "../components/UsdcWrapper/UsdcWrapper";
-import { WifiSpeaker1 } from "../icons/WifiSpeaker1";
-import { WiredController } from "../icons/WiredController";
-import { Xmr } from "../icons/Xmr";
-import { XmrWrapper } from "../components/XmrWrapper/XmrWrapper";
 import "../styles/style.css";
 import WalletTotal from "../components/WalletTotal";
 import '../styles/styleguide.css';
-import { useEffect, useState } from 'react';
 import Line24 from '../assets/img/line-24.svg'
+import WalletSearch from "../components/WalletSearch";
+import WalletBreakdown from "../components/WalletBreakdown";
+import MaskGroup from "../assets/img/mask-group.png"
+
 
 
 
@@ -42,14 +20,22 @@ function Wallet () {
         <Blur circleClassName="blur-instance" className="blur-5" />
         <Blur circleClassName="design-component-instance-node" className="blur-5-instance" />
       </div> 
-      {/* <img className="vector" alt="Vector" src="image.svg" />
-          <Blob11 className="blob" color="#A3F49D" />
-          <img className="img" alt="Group" src="group.png" />
-          <img className="vector-2" alt="Vector" src="vector.svg" />
-          <IconComponentNode className="blob-1" />  */}
         <div className="wallet-display-wrapper">
+          <Blob11 className="blob" color="#A3F49D" />
             <WalletTotal />
           <img className="line" alt="Line" src={Line24} />
+            <WalletSearch />
+            <br></br>
+            <WalletBreakdown />
+        </div>
+        <div className="settings-wrapper">
+            <img className="mask-group" alt="Mask group" src={MaskGroup} />
+            <div className="frame-25">
+              <div className="heading">Wallet Name</div>
+              <div className="heading-wrapper">
+                <div className="heading-6">settings</div>
+              </div>
+            </div>
         </div>
    </div>
   );
